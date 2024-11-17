45d8750c5e319e47f665c55f4da529b5e3d0df1f
Adrian Soovary <adrian.soovary@otto.de>
2024-11-17 12:02:21 +0100

    feat(frontend): adjust custom api route to use our custom backend

--- frontend/app/api/chat/custom/route.ts
+++ frontend/app/api/chat/custom/route.ts
@@ -1,47 +1,24 @@
-import { Database } from "@/supabase/types"
 import { ChatSettings } from "@/types"
-import { createClient } from "@supabase/supabase-js"
 import { OpenAIStream, StreamingTextResponse } from "ai"
 import { ServerRuntime } from "next"
-import OpenAI from "openai"
-import { ChatCompletionCreateParamsBase } from "openai/resources/chat/completions.mjs"
 
 export const runtime: ServerRuntime = "edge"
 
 export async function POST(request: Request) {
   const json = await request.json()
-  const { chatSettings, messages, customModelId } = json as {
+  const { messages } = json as {
     chatSettings: ChatSettings
     messages: any[]
     customModelId: string
   }
 
   try {
-    const supabaseAdmin = createClient<Database>(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.SUPABASE_SERVICE_ROLE_KEY!
-    )
-
-    const { data: customModel, error } = await supabaseAdmin
-      .from("models")
-      .select("*")
-      .eq("id", customModelId)
-      .single()
-
-    if (!customModel) {
-      throw new Error(error.message)
-    }
-
-    const custom = new OpenAI({
-      apiKey: customModel.api_key || "",
-      baseURL: customModel.base_url
-    })
-
-    const response = await custom.chat.completions.create({
-      model: chatSettings.model as ChatCompletionCreateParamsBase["model"],
-      messages: messages as ChatCompletionCreateParamsBase["messages"],
-      temperature: chatSettings.temperature,
-      stream: true
+    const response = await fetch("http://127.0.0.1:5000/ask", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ query: messages[messages.length - 1].content })
     })
 
     const stream = OpenAIStream(response)
